dec96407649d36e2c47d17dbfa280a4d244d78b0
Aizam Jigjidsuren <aizamjig@gmail.com>
2020-04-06 18:07:18 -0700

    Create users data generator for Postgres.

---
+++ database/seeders/Postgres/users.js
@@ -0,0 +1,53 @@
+const cliProgress = require('cli-progress');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const faker = require('faker');
+
+const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+
+const usersWriter = createCsvWriter({
+    path: './users.csv',
+    header: [
+        {id: 'firstname', title: 'FIRST NAME'},
+        {id: 'lastname', title: 'LAST NAME'},
+        {id: 'follower_count', title: 'FOLLOWER COUNT'},
+        {id: 'created', title: 'DATE CREATED'},
+    ]
+});
+
+let userCount = 1000;
+let randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const generateUsers = () => {
+    const users = [];
+    for (var i = 0; i < userCount; i++ ) {
+        const user = {};
+        const date = faker.date.past().toString()
+        user.firstname = faker.name.firstName();
+        user.lastname = faker.name.lastName();
+        user.follower_count = randomInt(1, 500);
+        user.created = date.slice(0, 33);
+        users.push(user);
+    }
+    return users;
+
+}
+
+let chunks = 0;
+
+const addUsers = () => {
+    if (chunks < 1000) {
+        chunks += 1;
+        const users = generateUsers();
+        usersWriter.writeRecords(users)       // returns a promise
+            .then(() => {
+                bar.increment();
+                addUsers();
+            });
+    } else {
+        bar.stop();
+        console.timeEnd();
+    }
+}
+console.time();
+bar.start(1000, 0);
+addUsers();
